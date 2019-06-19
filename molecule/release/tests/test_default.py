27dccc5142c3ede8cdae8924471f8e25ce7c884b
Sebastien Besson <seb.besson@gmail.com>
2019-06-19 21:19:18 +0100

    Skip symlink creation on pre-releases

--- molecule/release/tests/test_default.py
+++ molecule/release/tests/test_default.py
@@ -24,12 +24,22 @@ def test_permissions(host, base_folder):
 @pytest.mark.parametrize('base_folder', [DOWNLOADS_URL, DOCS_URL])
 def test_symlinks(host, base_folder):
     v = host.ansible.get_variables()
+    hostname = host.backend.get_hostname()
     f = host.file('%s/component/3.2' % base_folder)
-    assert f.is_symlink
-    assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
+    if hostname == 'release':
+        assert f.is_symlink
+        assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
+    elif hostname == 'prelease':
+        assert not f.exists
     f = host.file('%s/component/3' % base_folder)
     assert f.is_symlink
-    assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
+    if hostname == 'release':
+        assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
+    elif hostname == 'prelease':
+        assert f.linked_to == '%s/component/3.1.8' % base_folder
     f = host.file('%s/component/latest' % base_folder)
     assert f.is_symlink
-    assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
+    if hostname == 'release':
+        assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
+    elif hostname == 'prelease':
+        assert f.linked_to == '%s/component/3.1.8' % base_folder
