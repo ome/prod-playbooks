e9d64c16f82ca7e4fa87e22aa2d89bca5c77c9f1
Sebastien Besson <seb.besson@gmail.com>
2019-06-19 13:34:51 +0100

    Add molecule scenario for release-acceptance playbook

---
+++ molecule/release/tests/test_default.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+DOWNLOADS_URL = "/uod/idr/www/downloads.openmicroscopy.org"
+DOCS_URL = "/uod/idr/www/docs.openmicroscopy.org"
+
+
+@pytest.mark.parametrize('base_folder', [DOWNLOADS_URL, DOCS_URL])
+def test_htaccess(host, base_folder):
+    assert not host.file('%s/component/3.2.10/.htaccess' % base_folder).exists
+
+
+@pytest.mark.parametrize('base_folder', [DOWNLOADS_URL, DOCS_URL])
+def test_permissions(host, base_folder):
+    f = host.file('%s/component/3.2.10' % base_folder)
+    assert f.exists
+    assert f.user == 'root'
+    assert oct(f.mode) == '01555'
