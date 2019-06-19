3d234b59f1a863d669626c71d7197edce3eb5805
Sebastien Besson <seb.besson@gmail.com>
2019-06-19 19:57:39 +0100

    Test both release and prelease

--- molecule/release/tests/test_default.py
+++ molecule/release/tests/test_default.py
@@ -10,13 +10,12 @@ DOCS_URL = "/uod/idr/www/docs.openmicroscopy.org"
 
 
 @pytest.mark.parametrize('base_folder', [DOWNLOADS_URL, DOCS_URL])
-def test_htaccess(host, base_folder):
-    assert not host.file('%s/component/3.2.0/.htaccess' % base_folder).exists
-
-
-@pytest.mark.parametrize('base_folder', [DOWNLOADS_URL, DOCS_URL])
 def test_permissions(host, base_folder):
-    f = host.file('%s/component/3.2.0' % base_folder)
+    v = host.ansible.get_variables()
+    assert not host.file('%s/component/%s/.htaccess' % (
+        base_folder, v['version'])).exists
+
+    f = host.file('%s/component/%s' % (base_folder, v['version']))
     assert f.exists
     assert f.user == 'root'
     assert oct(f.mode) == '01555'
@@ -24,12 +23,13 @@ def test_permissions(host, base_folder):
 
 @pytest.mark.parametrize('base_folder', [DOWNLOADS_URL, DOCS_URL])
 def test_symlinks(host, base_folder):
+    v = host.ansible.get_variables()
     f = host.file('%s/component/3.2' % base_folder)
     assert f.is_symlink
-    assert f.linked_to == '%s/component/3.2.0' % base_folder
+    assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
     f = host.file('%s/component/3' % base_folder)
     assert f.is_symlink
-    assert f.linked_to == '%s/component/3.2.0' % base_folder
+    assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
     f = host.file('%s/component/latest' % base_folder)
     assert f.is_symlink
-    assert f.linked_to == '%s/component/3.2.0' % base_folder
+    assert f.linked_to == '%s/component/%s' % (base_folder, v['version'])
