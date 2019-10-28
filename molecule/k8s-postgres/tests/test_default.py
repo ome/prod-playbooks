891453cf17fc87bebb3ee9a9dad9accbf6d0746d
Sebastien Besson <seb.besson@gmail.com>
2019-10-28 08:36:42 +0000

    Fix k8 tests for Molecule 2

--- molecule/k8s-postgres/tests/test_default.py
+++ molecule/k8s-postgres/tests/test_default.py
@@ -5,12 +5,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_service_running_and_enabled(Service):
-    assert Service('postgresql-9.6').is_running
-    assert Service('postgresql-9.6').is_enabled
+def test_service_running_and_enabled(host):
+    assert host.service('postgresql-9.6').is_running
+    assert host.service('postgresql-9.6').is_enabled
 
 
-def test_dbs(Command):
-    out = Command.check_output(
+def test_dbs(host):
+    out = host.check_output(
         'PGPASSWORD=idr-redmine psql -hlocalhost -Uidr-redmine -l -tA')
     assert 'idr-redmine|idr-redmine|UTF8|' in out
