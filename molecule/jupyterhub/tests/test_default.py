f446f4ed5e35c9402a46c3fd26fb2665cffd385e
Simon Li <spli@dundee.ac.uk>
2020-02-25 13:48:39 +0000

    Update jupyterhub test

--- molecule/jupyterhub/tests/test_default.py
+++ molecule/jupyterhub/tests/test_default.py
@@ -14,9 +14,6 @@ def test_services_running_and_enabled(host):
 def test_tmplogin(host):
     out = host.check_output(
         'curl --dump-header - -k https://localhost/training/hub/tmplogin')
-    pattern = re.compile(
-        r'location: /training/user/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}')
-    assert any(pattern.search(line) for line in out.splitlines())
-
+    assert 'location: /training/hub/spawn' in out
 
 # TODO: check notebook container runs after login
