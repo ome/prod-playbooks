d0190712502c11d732271b35367b45a8bb6e9590
Simon Li <spli@dundee.ac.uk>
2021-01-12 13:42:44 +0000

    ome-dockr-prod1.openmicroscopy.org prometheus grafana

--- molecule/docker-prod/tests/test_default.py
+++ molecule/docker-prod/tests/test_default.py
@@ -7,25 +7,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_targets(host):
-    out = host.check_output('curl -f http://localhost:9090/api/v1/targets')
+    out = host.check_output(
+        'curl -k -f --user admin:monitoring '
+        'https://localhost/prometheus/api/v1/targets')
     d = json.loads(out)
     assert d['status'] == 'success'
     assert d['data']['droppedTargets'] == []
     unique_instances = set(
         t['labels']['instance'] for t in d['data']['activeTargets'])
-    assert unique_instances == {
-        'idr-analysis.openmicroscopy.org:443',
+    assert len({
+        'node.example.org:443',
+        'pg.example.org:443',
+        'omeroserver.example.org:443',
+        'omeroweb.example.org:443',
         'idr.openmicroscopy.org:443',
         'idr1.openmicroscopy.org:443',
         'idr2.openmicroscopy.org:443',
         'localhost:9090',
-        'ns-web-pub.openmicroscopy.org:443',
-        'ns-web.openmicroscopy.org:443',
-        'ome-demoserver.openmicroscopy.org:443',
-        'ome-dockr-prod1.openmicroscopy.org:9090',
-        'ome-dockr-prod1.openmicroscopy.org:9100',
-        'ome-dundeeomero.openmicroscopy.org:443',
-        'outreach.openmicroscopy.org:443',
-        'pub-omero.openmicroscopy.org:443',
-        'workshop.openmicroscopy.org:443',
-    }
+    }.difference(unique_instances)) == 0
